88056cab39789eccf34da2279dc86c9ced418d45
Martín López <mlopez@flowingcode.com>
2021-11-08 12:07:03 -0300

    feat: replace applayout with fusion layout

--- frontend/views/main-layout.ts
+++ frontend/views/main-layout.ts
@@ -1,16 +1,16 @@
 import '@vaadin/vaadin-app-layout';
-import { AppLayoutElement } from '@vaadin/vaadin-app-layout';
-import '@vaadin/vaadin-app-layout/vaadin-drawer-toggle';
+import "@vaadin/flow-frontend/fc-applayout/fc-fusion-layout";
+import { FusionLayout } from "@vaadin/flow-frontend/fc-applayout/fc-fusion-layout";
 import '@vaadin/vaadin-avatar/vaadin-avatar';
 import '@vaadin/vaadin-context-menu';
 import '@vaadin/vaadin-tabs';
 import '@vaadin/vaadin-tabs/vaadin-tab';
 import '@vaadin/vaadin-template-renderer';
 import { html } from 'lit';
+import { query } from 'lit-element';
 import { customElement } from 'lit/decorators.js';
 import { router } from '../index';
 import { views } from '../routes';
-import { appStore } from '../stores/app-store';
 import { Layout } from './view';
 
 interface RouteInfo {
@@ -21,50 +21,27 @@ interface RouteInfo {
 
 @customElement('main-layout')
 export class MainLayout extends Layout {
+
+  @query("fc-fusion-layout")
+  layout!: FusionLayout;
+
   render() {
     return html`
-      <vaadin-app-layout primary-section="drawer">
-        <header class="bg-base border-b border-contrast-10 box-border flex h-xl items-center w-full" slot="navbar">
-          <vaadin-drawer-toggle aria-label="Menu toggle" class="text-secondary" theme="contrast"></vaadin-drawer-toggle>
-          <h1 class="m-0 text-l">${appStore.currentViewTitle}</h1>
-        </header>
-        <section class="flex flex-col items-stretch max-h-full min-h-full" slot="drawer">
-          <h2 class="flex items-center h-xl m-0 px-m text-m">${appStore.applicationName}</h2>
-          <nav aria-labelledby="views-title" class="border-b border-contrast-10 flex-grow overflow-auto">
-            <h3 class="flex items-center h-m mx-m my-0 text-s text-tertiary" id="views-title">Views</h3>
-            <ul class="list-none m-0 p-0">
-              ${this.getMenuRoutes().map(
-                (viewRoute) => html`
-                  <li>
-                    <a
-                      ?highlight=${viewRoute.path == appStore.location}
-                      class="flex mx-s p-s relative text-secondary"
-                      href=${router.urlForPath(viewRoute.path)}
-                    >
-                      <span class="${viewRoute.icon} me-s text-l"></span>
-                      <span class="font-medium text-s">${viewRoute.title}</span>
-                    </a>
-                  </li>
-                `
-              )}
-            </ul>
-          </nav>
-          <footer class="flex items-center my-s px-m py-xs"></footer>
-        </section>
-        <slot></slot>
-      </vaadin-app-layout>
-    `;
+    <fc-fusion-layout swipeOpen fixed 
+                      userName="A given user" title="Social App"
+                      appLogo="images/logo.png"
+                      profilePicture="icons/icon.png">
+		  <slot></slot>
+	  </fc-fusion-layout>`;
   }
 
   connectedCallback() {
     super.connectedCallback();
     this.classList.add('block', 'h-full');
-    this.reaction(
-      () => appStore.location,
-      () => {
-        AppLayoutElement.dispatchCloseOverlayDrawerEvent();
-      }
-    );
+  }
+
+  firstUpdated() {
+    this.layout.router = router;
   }
 
   private getMenuRoutes(): RouteInfo[] {
